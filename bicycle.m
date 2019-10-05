7d6b50367ffc17f343d83da54cb61972ae33b0d1
Rahul Purohit <42001108+rahulpurohit29@users.noreply.github.com>
2019-10-05 20:13:47 +0530

    The program to model an movement of bicycle.
    This program has also considered the static friction to model motion and velocity.

---
+++ bicycle.m
@@ -0,0 +1,25 @@
+clc;
+clear all;
+
+init_vel=5;
+time_p=300;
+dt=1;
+npoints=time_p/dt;
+
+time=zeros(npoints,1);
+speed=zeros(npoints,1);
+
+time(1)=0;
+speed(1)=init_vel;
+mass=50;
+power=400;
+
+constant=0.5;
+density=1.225;
+area=0.33;
+
+for step=1:npoints-1
+  speed(step+1)=speed(step)+power*density/(mass*speed(step))-constant*density*area*(speed(step).^2)/(2*mass);
+  time(step+1)=time(step)+dt;
+endfor
+plot(time,speed);
